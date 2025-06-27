51eb87a08cd9a4584786a57c64670333353788fb
Reovirus <pitikovegor@gmail.com>
2025-06-27 10:18:11 +0200

    faster chains (old was >10 mins on hg18.danRer3.all.chain.gz)

--- src/readers/chain_reader.py
+++ src/readers/chain_reader.py
@@ -1,6 +1,7 @@
 from typing import List, Dict, Any, BinaryIO, TextIO, Union
 import gzip
 import polars as pl
+import io
 
 from src._utils import (
     TYPE_POLARS_INTEGER,
@@ -30,11 +31,99 @@ _BLOCK_SCHEMA = { col : TYPE_POLARS_INTEGER for col in _block_cols}
 
 
 class ChainReader:
-    def __init__(self, chain_file: Union[BinaryIO, TextIO, str]):
+    def __init__(self, chain_file: Union[BinaryIO, TextIO, str], use_buffered: bool = False, buffer_size: int = 4 * 1024 * 1024):
         self.__chain_file = chain_file
-        self.__read_chain()
+        if use_buffered:
+            self.__read_chain_buffered(buffer_size)
+        else:
+            self.__read_chain()
 
-    #its dangerous to load the full chain file twice. add economy and slow mode with rows iterator
+    def __read_chain_buffered(self, buffer_size: int = 4 * 1024 * 1024):
+        chains_lines = []
+        blocks_lines = []
+        index_chain_block = -1
+        is_block = False
+
+        if isinstance(self.__chain_file, str):
+            gz = gzip.open(self.__chain_file, "rb")
+        else:
+            gz = self.__chain_file 
+        buf = io.BufferedReader(gz, buffer_size=buffer_size)
+
+        for raw in buf:
+            line = raw.rstrip(b"\r\n")
+            if not line or line.startswith(b"#"):
+                continue
+
+            if line.startswith(b"chain"):
+                index_chain_block += 1
+                is_block = True
+                chains_lines.append(line + b" " + str(index_chain_block).encode())
+                blocks_lines.append(f"{index_chain_block}\t0\t0\t0".encode())
+                continue
+
+            if is_block:
+                if line.startswith(b"chain") or line.startswith(b"#"):
+                    is_block = False
+                    continue
+                blocks_lines.append(str(index_chain_block).encode() + b"\t" + line)
+
+        chains_df = pl.DataFrame({"raw": chains_lines}).with_columns(
+            pl.col(
+                "raw"
+            ).cast(pl.String).str.split_exact(
+                " ", len(_CHAIN_SCHEMA.keys()) - 1
+            ).struct.rename_fields(
+                list(_CHAIN_SCHEMA.keys())
+            ).alias("blk")
+        ).unnest(
+            "blk"
+        ).cast(_CHAIN_SCHEMA)
+
+        blocks_df = pl.DataFrame({"raw": blocks_lines}).with_columns(
+            pl.col(
+                "raw"
+            ).cast(pl.String).str.split_exact(
+                "\t", len(_BLOCK_SCHEMA.keys()) - 1
+            ).struct.rename_fields(
+                list(_BLOCK_SCHEMA.keys())
+            ).alias("blk")
+        ).unnest(
+            "blk"
+        ).cast(_BLOCK_SCHEMA).fill_null(0)
+
+        blocks_df.drop_in_place("raw")
+        chains_df.drop_in_place("raw")
+
+        blocks_df = blocks_df.join(chains_df, on="index_chain_block")
+        blocks_df = blocks_df.with_columns(
+            pl.col("size_aln").shift(-1).over("index_chain_block").fill_null(0).alias("size_aln")
+        )
+        blocks_df = blocks_df.with_columns(
+            (pl.col("size_aln").cum_sum().over("index_chain_block")
+             + pl.col("gap_aln_source").cum_sum().over("index_chain_block")
+             + pl.col("start_chain_source")).alias("end_aln_source"),
+            (pl.col("size_aln").cum_sum().over("index_chain_block")
+             + pl.col("gap_aln_target").cum_sum().over("index_chain_block")
+             + pl.col("start_chain_target")).alias("end_aln_target")
+        )
+        blocks_df = blocks_df.with_columns(
+            (pl.col("end_aln_source") - pl.col("size_aln")).alias("start_aln_source"),
+            (pl.col("end_aln_target") - pl.col("size_aln")).alias("start_aln_target")
+        )
+        blocks_df = blocks_df.filter(pl.col("size_aln") > 0)
+        blocks_df = blocks_df.select([
+            'name_chain_source', 'start_aln_source', 'end_aln_source', 'strand_chain_source',
+            'name_chain_target', 'start_aln_target', 'end_aln_target', 'strand_chain_target',
+            'size_aln', 'start_chain_source', 'end_chain_source', 'size_chain_source',
+            'start_chain_target', 'end_chain_target', 'size_chain_target',
+            'score', 'index_chain', 'index_chain_block'
+        ])
+
+        self.__chains = chains_df
+        self.__blocks = blocks_df
+
+
     def __read_chain(self):
         if isinstance(self.__chain_file, str):
             with gzip.open(self.__chain_file, 'rb') as f:
@@ -45,6 +134,9 @@ class ChainReader:
         lines = file_content.splitlines()
         chains_data = b''
         blocks_data = b''
+
+        chains_data = []
+        blocks_data = []
 
         i = 0
         index_chain_block = 0
@@ -57,33 +149,48 @@ class ChainReader:
 
             if line.startswith(b'chain'):
                 header_line = line
-                chains_data += header_line + f' {index_chain_block}\n'.encode('utf-8')
+                chains_data.append(header_line + f' {index_chain_block}'.encode('utf-8'))
                 i += 1
 
-                blocks_data += f'{index_chain_block}\t'.encode('utf-8') + b'0\t0\t0\n'
+                blocks_data.append(f'{index_chain_block}\t'.encode('utf-8') + b'0\t0\t0')
                 while i < len(lines):
                     line = lines[i]
                     if line.startswith(b'chain') or line.startswith(b'#') or line.strip() == b"":
                         break
-                    blocks_data += f'{index_chain_block}\t'.encode('utf-8') + line + b'\n'
+                    blocks_data.append(f'{index_chain_block}\t'.encode('utf-8') + line)
                     i += 1
                 
                 index_chain_block += 1
 
             else:
                 i += 1
-        chains_df = pl.read_csv(
-                        chains_data,
-                        separator=" ",
-                        has_header=False,
-                        schema=_CHAIN_SCHEMA
-                    )
-        blocks_df = pl.read_csv(
-                        blocks_data,
-                        separator="\t",
-                        has_header=False,
-                        schema=_BLOCK_SCHEMA
-                    ).fill_null(0)
+
+        chains_df = pl.DataFrame({"raw": chains_data}).with_columns(
+            pl.col(
+                "raw"
+            ).cast(pl.String).str.split_exact(
+                " ", len(_CHAIN_SCHEMA.keys()) - 1
+            ).struct.rename_fields(
+                list(_CHAIN_SCHEMA.keys())
+            ).alias("blk")
+        ).unnest(
+            "blk"
+        ).cast(_CHAIN_SCHEMA)
+
+        blocks_df = pl.DataFrame({"raw": blocks_data}).with_columns(
+            pl.col(
+                "raw"
+            ).cast(pl.String).str.split_exact(
+                "\t", len(_BLOCK_SCHEMA.keys()) - 1
+            ).struct.rename_fields(
+                list(_BLOCK_SCHEMA.keys())
+            ).alias("blk")
+        ).unnest(
+            "blk"
+        ).cast(_BLOCK_SCHEMA).fill_null(0)
+
+        blocks_df.drop_in_place("raw")
+        chains_df.drop_in_place("raw")
 
         blocks_df = blocks_df.join(chains_df, on='index_chain_block')
 
@@ -115,7 +222,6 @@ class ChainReader:
             'score',
             'index_chain', 'index_chain_block'
             ])
-
         self.__chains = chains_df
         self.__blocks = blocks_df
 
@@ -135,7 +241,3 @@ class ChainReader:
     @chains.setter
     def value(self, new_value):
         raise ValueError("Chains cannot be set directly.")
-    
-
-    def chain_to_cheme(self, target_bins, source_bins, return_chroms=False):
-        pass
